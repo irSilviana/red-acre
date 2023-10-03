3bbbfbd1a6c651cbff5ecc6894248a228d30b9f2
Irfani Silviana <ir.silviana@gmail.com>
2023-10-04 01:34:18 +0200

    Add content html & style

--- wp-content/themes/red-acre/index.php
+++ wp-content/themes/red-acre/index.php
@@ -1,9 +1,56 @@
 <?php get_header(); ?>
-<div id="content" class="site-content">
+<div id="content" class="site-content container">
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
-            <section class="hero">Hero</section>
-            <section class="services">Services</section>
+            <section class="hero container row d-flex align-items-start align-items-lg-center">
+                <div class="col-sm-12 col-lg-6 col-xl-5 col-xxl-4 mb-5 pb-5">
+                    <h1>Tighter Spreads & Faster Execution</h1>
+                    <p class="sub-heading">With Forex Bros you trade with the best in the biz! With quality execution and some of the best trading conditions in the industry, your trading experience will be none other than epic.</p>
+                    <ul>
+                        <li>No dealing desk or requotes</li>
+                        <li>Commissions as low as $0</li>
+                    </ul>
+                </div>
+            </section>
+            <section class="services container">
+                <div class="row">
+                    <div class="col-12">
+                        <h2>Tools to Compliment Your Trading</h2>
+                        <p class="sub-heading text-center py-5">Access news & economic calendar, technical indicators & charts, Forex Calculators, educational resources and many more tools to help you excel.</p>
+                    </div>
+                    <div class="col-12">
+                        <div class="row d-flex flex-wrap flex-row justify-content-between services-items">
+                            <div class="col-12 col-lg-3 text-center service-item">
+                                <img src="/wp-content/uploads/2023/10/economic-calendar.png" alt="">
+                                <h3>
+                                    Economic Calendar
+                                </h3>
+                                <p>
+                                    Access all major events & economic news that impact financial markets.
+                                </p>
+                            </div>
+                            <div class="col-12 col-lg-3 text-center service-item">
+                                <img src="/wp-content/uploads/2023/10/trading-calculator.png" alt="">
+                                <h3>
+                                    Trading Calculators
+                                </h3>
+                                <p>
+                                    Make trading calculations in real-time & forecast the potential growth of your account.
+                                </p>
+                            </div>
+                            <div class="col-12 col-lg-3 text-center service-item">
+                                <img src="/wp-content/uploads/2023/10/educational-resources.png" alt="">
+                                <h3>
+                                    Educational Resources
+                                </h3>
+                                <p>
+                                    From top tips & trading psychology to more ways to earn, we make learning more fun.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     </div>
 </div>
